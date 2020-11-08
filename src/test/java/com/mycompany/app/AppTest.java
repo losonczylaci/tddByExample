7c433045ac878b6ea30999a67905a59c827e9d0f
Laszlo Losonczy <losonczylaci@gmail.com>
2020-11-08 00:31:40 +0000

    worked

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -3,21 +3,17 @@ package com.mycompany.app;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
-public class AppTest
-{
+public class AppTest {
     public AppTest() {
     }
 
     @Test
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true);
     }
 
     @Test
-    public void testMore()
-    {
-        assertTrue( true );
+    public void testMore() {
+        assertTrue(true);
     }
 }
